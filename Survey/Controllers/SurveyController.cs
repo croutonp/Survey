046291d96763aa165ce45eece4f6a567c93e3621
croutonp <cgpasaoa@gmail.com>
2023-09-05 01:25:42 -0400

    init

---
+++ Survey/Controllers/SurveyController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewWeb.Controllers;
+
+public class SurveyController : Controller
+{
+    // [HttpGet]
+    // [Route("")]
+    // public string Index()
+    // {
+    //     return "Hello from the controller";
+    // }
+
+    // [HttpGet("results")]
+    // public string Two()
+    // {
+    //     return "Page Two";
+    // }
+
+    // [HttpGet("params/{id}/{name}")]
+    // public string Params(int id, string name)
+    // {
+    //     return $"{name} is id {id}";
+    // }
+
+    // [HttpGet("view")]
+    // public ViewResult SurveyView()
+    // {
+    //     return View("SurveyView");
+    // }
+
+    [HttpGet("")]
+    public ViewResult SurveyView()
+    {
+        return View();
+    }
+
+    [HttpGet("results")]
+    public ViewResult ResultView()
+    {
+        return View();
+    }
+
+    [HttpPost("submit")]
+    public IActionResult Process(string name, string location, string language, string comment)
+    {
+        ViewBag.Name = name;
+        ViewBag.Location = location;
+        ViewBag.Language = language;
+        ViewBag.Comment = comment;
+        return View("ResultView");
+    }
+}
